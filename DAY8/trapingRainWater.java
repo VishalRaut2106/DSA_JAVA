e8660c3f5fd3fbca0a80e670fd6e7ac8c6f09153
VishalRaut2106 <rautv2005@gmail.com>
2025-06-19 21:30:26 +0530

    DAY8_VIMP

--- DAY8/trapingRainWater.java
+++ DAY8/trapingRainWater.java
@@ -12,20 +12,21 @@ public class trapingRainWater {
 
         // calculate right max boundary -array
         int rightmax[] = new int[n];
-        rightmax[n - 1] = height[n - 1];
-        for (int i = n - 2; i >= 0; i--) {
-            rightmax[i] = Math.max(height[i], rightmax[i + 1]);
+        rightmax[n -1]= height [ n -1];
+        for (int i=n-2; i>=0 ; i--){
+            rightmax[i] = Math.max(height[i], rightmax[i+1]);
         }
-        int trappedeWater = 0;
+        int  trappedeWater = 0;
         // loop
-        for (int i = 0; i < n; i++) {
-            // water lavele = min (leftmax , rightmax)
-            int waterLevel = Math.min(leftMax[i], rightmax[i]);
-            // trapped water = water level - height[i]
-            trappedeWater += waterLevel - height[i];
-
-        }
-        return trappedeWater;
+            for(int i=0; i<n;i++){
+                // water lavele = min (leftmax , rightmax)
+                int waterLevel = Math.min( leftMax[i],rightmax[i]);
+                    // trapped water = water level - height[i]
+                    trappedeWater += waterLevel - height[i] ;
+                
+                
+                }
+                return  trappedeWater;
     }
 
     public static void main(String[] args) {
@@ -34,5 +35,6 @@ public class trapingRainWater {
 
     }
 }
+
 
 // 11
